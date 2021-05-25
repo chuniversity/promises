f8a9a77991bcbcc63da73d3f3626f44285a63ae0
Chuniversity <contact@chunis.org>
2021-05-25 11:35:16 -0400

    update

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -9,12 +9,23 @@ var request = require('needle');
 var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+var pluckFirstLineFromFileAsync = function (filePath) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        var lines = data.toString().split(/\r?\n/);
+        resolve(lines[0]);
+      }
+    });
+  });
+
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCodeAsync = function(url) {
+var getStatusCodeAsync = function (url) {
   // TODO
 };
 
